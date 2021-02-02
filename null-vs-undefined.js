efd144d0bea0222f02fcb87ca929035b7a39b9be
moshiur9185 <moshiur9185@gmail.com>
2021-02-02 10:52:24 +0600

    undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+let name;
+console.log(name);
+
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+const result = add(02, 07);
+console.log(result);
+
+const man = {name:"Jontu", phone:4568489};
+console.log(man.address);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2,6,7,8];
+console.log(ages[12]);
