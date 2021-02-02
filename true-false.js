e8b47e2c41c31e42fe5be19b91404fea9710c618
moshiur9185 <moshiur9185@gmail.com>
2021-02-02 09:47:22 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,20 @@
+// false value.....
+
+// 1. false
+// 2. 0
+// 3. ""
+// 4. undefined
+// 5. null
+// 6. NaN
+
+// truthy values.....
+// '0', " ", []
+
+let name = 0;
+console.log(name);
+if(name){
+    console.log('this is true');
+}
+else{
+    console.log('this is false');
+}
